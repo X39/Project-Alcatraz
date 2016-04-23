ca8d18a111fa1ec520b986bac2da85b83dbeb96f
Marco.Silipo <m.silipo@web.de>
2016-04-23 14:11:14 +0200

    added way to get strings of functions/global vars

--- X39_XMed_Scripting/defaultSqf.hpp
+++ X39_XMed_Scripting/defaultSqf.hpp
@@ -1,7 +1,10 @@
 #include "x\x39\ExtensiveMedicalsystem\scripting\default.hpp"
 #include "x\x39\ExtensiveMedicalsystem\scripting\exceptionIds.hpp"
+#define STR(EXT) #EXT
 #define FNC(EXT) X39_XMed_fnc_##EXT
 #define GVAR(EXT) X39_XMed_var_##EXT
+#define FNCS(EXT) STR(FNC(EXT))
+#define GVARS(EXT) STR(GVAR(EXT))
 
 #ifdef DEBUG
     //If in debug mode, enable tracing & printing call arguments
